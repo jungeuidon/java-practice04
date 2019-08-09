5469683bb67cb8d4453333963bb6266231d7e1db
jungeuidon <mantopuma@naver.com>
2019-08-09 20:11:13 +0900

    1번 완료 overloading (다형성)

--- src/prob01/Printer.java
+++ src/prob01/Printer.java
@@ -2,5 +2,23 @@ package prob01;
 
 public class Printer{
 
+	public Printer() {
+	}
+	
+	public void println(int word) {
+		System.out.println(word);
+	}
+	public void println(boolean word) {
+		System.out.println(word);
+	}
+	public void println(double word) {
+		System.out.println(word);
+	}
+	public void println(String word) {
+		System.out.println(word);
+	}
+	public void println(int word, boolean word1, double word2, String word3) {
+		System.out.println(word);
+	}
 	
 }

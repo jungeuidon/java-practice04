7b4ddded599bf6e43beffcd7c328b26c1629c160
jungeuidon <mantopuma@naver.com>
2019-08-09 20:28:04 +0900

    2번 완료

--- src/prob02/Book.java
+++ src/prob02/Book.java
@@ -6,7 +6,48 @@ public class Book {
 	private String author;
 	private int stateCode;
 	
+	
+	
+	public int getBookNo() {
+		return bookNo;
+	}
 
+
+	public void setBookNo(int bookNo) {
+		this.bookNo = bookNo;
+	}
+
+
+	public String getTitle() {
+		return title;
+	}
+
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+
+	public String getAuthor() {
+		return author;
+	}
+
+
+	public void setAuthor(String author) {
+		this.author = author;
+	}
+
+
+	public Book(int bookNo,String title,String author) {
+		this.bookNo = bookNo;
+		this.title = title;
+		this.author = author;
+	}
+	
+	public void rent() {
+		stateCode= 0 ;
+		System.out.println( getTitle() +  "이 대여되었습니다.");
+	}
 	
 	public void print() {
 		System.out.println(
@@ -14,4 +55,6 @@ public class Book {
 			", 작가:" + author +
 			", 대여 유무:" + ( stateCode == 1 ? "재고있음" : "대여중" ) );
 	}
+	
+	
 }
--- src/prob02/BookShop.java
+++ src/prob02/BookShop.java
@@ -30,7 +30,7 @@ public class BookShop {
 				break;
 			}
 		}
-		
+
 		// (2) Book 객체의 정보를 출력
 		System.out.println("*****도서 정보 출력하기******");
 		displayBookInfo( books );
